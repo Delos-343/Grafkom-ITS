899e9e8d92b21f815c888207e8eeec4b927e0d1b
Delos-343 <mfachry.handoko@gmail.com>
2022-09-06 14:36:42 +0700

    added codes to all files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function main() {
+    const canvas = document.querySelector('#paintJob');
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      alpha: true,
+    });
+
+    const shader = "shader";
+}
+    
