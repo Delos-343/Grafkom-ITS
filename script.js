3f3cc53991aac762c50050ce0869ce707101264b
Delos-343 <mfachry.handoko@gmail.com>
2022-09-13 14:52:39 +0700

    trying again

--- script.js
+++ script.js
@@ -33,6 +33,9 @@ function main() {
 
     // Frament Shader
     var fragmentShaderCode = `
+
+        precision mediump float;
+
         void main() {
             gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
 
